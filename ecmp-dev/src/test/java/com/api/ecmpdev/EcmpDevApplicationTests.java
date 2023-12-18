063ea5b7ab561a8fe5a7c4611ee64de509260714
SkyZ18 <114467762+SkyZ18@users.noreply.github.com>
2023-12-18 20:29:12 +0100

    init Backend

---
+++ ecmp-dev/src/test/java/com/api/ecmpdev/EcmpDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ecmpdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmpDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
