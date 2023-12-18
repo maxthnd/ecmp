49d2ab80cd2d099bbf608b9cae4e768d20ff9bde
maxthnd <schule.deckermaximilian@gmx.de>
2023-12-18 20:05:12 +0100

    frontend: refactored whole thing

--- ecmp-frontend/src/app/app.component.ts
+++ ecmp-frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {BrowserModule} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [BrowserModule, NgbModule, CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
