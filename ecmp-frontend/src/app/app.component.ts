a8ad21ebcb1b21142ec74eddae0a3ba6282fa5ab
SkyZ18 <114467762+SkyZ18@users.noreply.github.com>
2023-12-22 08:25:21 +0100

    Added Frontend

--- ecmp-frontend/src/app/app.component.ts
+++ ecmp-frontend/src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {BrowserModule} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BrowserModule, NgbModule, CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ecmp-frontend';
